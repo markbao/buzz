2bc63dbc54126fc31ef731b9f91cedfb6abd8dee
Mark Bao <mark@markbao.com>
2012-02-15 03:08:44 -0500

    monitoring

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 var mdns = require('mdns');
 
 var Buzz = function () {
-  this.clients = [];
+  this.clients = {};
 };
 
 Buzz.prototype.refreshClients = function () {
   var self = this;
 
-  self.clients = [];
+  self.clients = {};
   self.sbrowser = mdns.browseThemAll();
 
   self.sbrowser.on('serviceUp', function (s) {
     var browser = mdns.createBrowser(mdns[s.type.protocol](s.type.name));
    
     browser.on('serviceUp', function (cli) {
-      if (cli.port != 7331) {
+      if (cli.port == 7331) {
         var tempClient = {};
         tempClient.name = cli.name;
         tempClient.ip = cli.addresses[0];
         tempClient.host = cli.host;
-        self.clients.push(tempClient);
+        self.clients[tempClient.ip] = tempClient;
       }
     });
 
@@ -34,6 +34,46 @@ Buzz.prototype.refreshClients = function () {
   }, 2000);
 }
 
+Buzz.prototype.monitorClients = function () {
+  var self = this;
+
+  self.browser = mdns.createBrowser(mdns.tcp('http'));
+
+  self.browser.on('serviceUp', function(cli) {
+    if (cli.port == 7331) {
+      if (!self.clients[cli.addresses[0]]) {
+        console.log('up: ' + cli.addresses[0]);
+
+        // TODO: abstract
+        var tempClient = {};
+        tempClient.name = cli.name;
+        tempClient.ip = cli.addresses[0];
+        tempClient.host = cli.host;
+      } else {
+        self.clients[cli.addresses[0]].name = cli.name;
+      }
+    }
+  });
+
+  self.browser.on('serviceDown', function (cli) {
+    console.log('down: ' + cli.addresses[0]);
+    delete self.clients[cli.addresses[0]];
+  });
+
+  self.browser.start();
+}
+
+Buzz.prototype.broadcastClient = function (name) {
+  var self = this;
+  self.clientAd = mdns.createAdvertisement(mdns.tcp('http'), 7331);
+  self.clientAd.start();
+}
+
 var buzz = new Buzz();
 
 buzz.refreshClients();
+buzz.monitorClients();
+
+var args = process.argv.splice(2);
+
+buzz.broadcastClient(args[0]);
